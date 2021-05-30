73a49acd4bb2d79b05876a826e9b6c190ddb1ae9
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-30 09:18:27 -0400

    chore: commit inicial

---
+++ src/test/java/me/gabriel/testingstudy/TestingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabriel.testingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
