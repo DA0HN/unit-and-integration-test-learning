105b20e729c65545b960cdbbda2d9a4702461444
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-30 15:24:59 -0400

    test: garante que após cada teste todos os dados serão excluidos

--- src/test/java/me/gabriel/testingstudy/domain/student/StudentRepositoryTest.java
+++ src/test/java/me/gabriel/testingstudy/domain/student/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.gabriel.testingstudy.domain.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,6 +19,10 @@ class StudentRepositoryTest {
   @Autowired
   private StudentRepository sut;
 
+  @AfterEach
+  void tearDown() {
+    sut.deleteAll();
+  }
 
   @Test
   void itShouldCheckIfStudentEmailAlreadyRegistered() {
