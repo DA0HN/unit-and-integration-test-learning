ae03ae1821a5381d97468b36a5ef429d712cf993
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-30 14:25:19 -0400

    test: exemplo de teste utilizando JUnit 5 + AssertJ

--- src/test/java/me/gabriel/testingstudy/TestingStudyApplicationTests.java
+++ src/test/java/me/gabriel/testingstudy/TestingStudyApplicationTests.java
@@ -3,11 +3,33 @@ package me.gabriel.testingstudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 @SpringBootTest
 class TestingStudyApplicationTests {
 
+  Calculator sut = new Calculator();
+
+
   @Test
-  void contextLoads() {
+  void itShouldAddTwoNumbers() {
+
+    // given
+    int numberOne = 20;
+    int numberTwo = 30;
+
+    // when
+    var result = sut.add(numberOne, numberTwo);
+
+    // then
+    var expected = 50;
+    assertThat(result).isEqualTo(expected);
   }
 
+  private static class Calculator {
+    int add(int a, int b) {
+      return a + b;
+    }
+  }
 }
