119cc736361590606d5a2a738b9228c437dd04ca
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-30 10:10:32 -0400

    feat: adiciona camada de serviço para Student

---
+++ src/main/java/me/gabriel/testingstudy/domain/student/StudentService.java
@@ -0,0 +1,39 @@
+package me.gabriel.testingstudy.domain.student;
+
+import lombok.AllArgsConstructor;
+import me.gabriel.testingstudy.domain.student.exception.StudentNotFoundException;
+import me.gabriel.testingstudy.exception.BadRequestException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by daohn on 30/05/2021
+ * @author daohn
+ * @since 30/05/2021
+ */
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+  private final StudentRepository repository;
+
+
+  public List<Student> findAll() {
+    return repository.findAll();
+  }
+
+  public void create(Student student) {
+    repository.findByEmail(student.getEmail())
+      .orElseThrow(() -> new BadRequestException("Email " + student.getEmail() + " already in use."));
+    repository.save(student);
+  }
+
+  public void deleteById(Long id) {
+    if(!repository.existsById(id)) {
+      throw new StudentNotFoundException("Student with id " + id + " does not exists");
+    }
+
+    repository.deleteById(id);
+  }
+}
