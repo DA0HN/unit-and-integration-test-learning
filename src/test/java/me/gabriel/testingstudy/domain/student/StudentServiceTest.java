c04da66f1f58e4bb6dc61f4687fc8fe44252c230
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-30 20:12:25 -0400

    test: garante que a instância de Student que será persistida no banco é o mesmo do argumento

--- src/test/java/me/gabriel/testingstudy/domain/student/StudentServiceTest.java
+++ src/test/java/me/gabriel/testingstudy/domain/student/StudentServiceTest.java
@@ -4,10 +4,12 @@ import me.gabriel.testingstudy.domain.student.exception.EmailAlreadyInUseExcepti
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static me.gabriel.testingstudy.domain.student.StudentFactory.makeStudent;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -46,10 +48,17 @@ class StudentServiceTest {
   void whenStudentDoesNotHaveRegisteredEmailThenShouldBeCreated() {
     var student = makeStudent();
 
+    when(studentRepository.isEmailAlreadyInUse(anyString())).thenReturn(false);
+
     sut.create(student);
 
+    var studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+    verify(studentRepository).save(studentArgumentCaptor.capture());
     verify(studentRepository, times(1)).isEmailAlreadyInUse(anyString());
     verify(studentRepository, times(1)).save(student);
+
+    assertThat(studentArgumentCaptor.getValue()).isEqualTo(student);
   }
 
   @Test
