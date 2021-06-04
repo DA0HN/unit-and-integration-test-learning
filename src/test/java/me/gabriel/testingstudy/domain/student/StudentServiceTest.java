af4cdba454a57fedc84a630aed40fcd895a66db0
Gabriel Honda <gabrielhondacba@gmail.com>
2021-06-03 22:27:39 -0400

    test: garante funcionamento correto do StudentMapper na classe StudentService

--- src/test/java/me/gabriel/testingstudy/domain/student/StudentServiceTest.java
+++ src/test/java/me/gabriel/testingstudy/domain/student/StudentServiceTest.java
@@ -5,11 +5,13 @@ import me.gabriel.testingstudy.domain.student.exception.StudentNotFoundException
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static me.gabriel.testingstudy.domain.student.StudentFactory.createValidStudent;
+import static me.gabriel.testingstudy.domain.student.StudentFactory.createValidStudentDto;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,57 +29,65 @@ class StudentServiceTest {
 
   @Mock
   private StudentRepository studentRepository;
+  @Mock
+  private StudentMapper studentMapper;
 
   private StudentService sut;
 
   private static final Long ID_VALID = 1L;
   private static final Long ID_INVALID = -1L;
   private static final Long ID_NULL = null;
+  private static final Class<Student> STUDENT_CLASS = Student.class;
+  private static final Class<StudentDto> STUDENT_DTO_CLASS = StudentDto.class;
 
   @BeforeEach
   void setUp() {
-    this.sut = new StudentServiceImpl(studentRepository);
+    this.sut = new StudentServiceImpl(studentRepository, studentMapper);
   }
 
   @Test
   void givenDatabaseWhenThereAreRegisteredStudentsThenShouldListAll() {
+    when(studentRepository.findAll()).thenReturn(List.of(createValidStudent()));
 
     sut.findAll();
 
     verify(studentRepository, times(1)).findAll();
+    verify(studentMapper, times(1)).fromStudentEntityToStudentDto(any(STUDENT_CLASS));
   }
 
   @Test
   void givenStudentWhenHasUnregisteredEmailThenShouldBeCreated() {
-    var student = createValidStudent();
+    var studentDto = createValidStudentDto();
 
     when(studentRepository.isEmailAlreadyInUse(anyString())).thenReturn(false);
+    when(studentRepository.save(isA(STUDENT_CLASS))).thenReturn(createValidStudent());
+    when(studentMapper.fromStudentDtoToStudentEntity(isA(STUDENT_DTO_CLASS))).thenReturn(createValidStudent());
+    when(studentMapper.fromStudentEntityToStudentDto(isA(STUDENT_CLASS))).thenReturn(createValidStudentDto());
 
-    sut.create(student);
+    sut.create(studentDto);
 
-    var studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
-
-    verify(studentRepository).save(studentArgumentCaptor.capture());
     verify(studentRepository, times(1)).isEmailAlreadyInUse(anyString());
-    verify(studentRepository, times(1)).save(student);
-
-    assertThat(studentArgumentCaptor.getValue()).isEqualTo(student);
+    verify(studentRepository, times(1)).save(any(STUDENT_CLASS));
+    verify(studentMapper, times(1)).fromStudentDtoToStudentEntity(any(STUDENT_DTO_CLASS));
+    verify(studentMapper, times(1)).fromStudentEntityToStudentDto(any(STUDENT_CLASS));
   }
 
   @Test
   void givenStudentWhenHaveRegisteredEmailThenShouldThrowEmailAlreadyInUseException() {
-
-    var student = createValidStudent();
-
+    when(studentMapper.fromStudentDtoToStudentEntity(any(STUDENT_DTO_CLASS))).thenReturn(createValidStudent());
     when(studentRepository.isEmailAlreadyInUse(anyString())).thenReturn(true);
+
+    var studentDto = createValidStudentDto();
 
     assertThrows(
       EmailAlreadyInUseException.class,
-      () -> sut.create(student)
+      () -> sut.create(studentDto)
     );
 
+    verify(studentMapper, times(1)).fromStudentDtoToStudentEntity(any(STUDENT_DTO_CLASS));
+    verify(studentMapper, never()).fromStudentEntityToStudentDto(any(STUDENT_CLASS));
     verify(studentRepository, times(1)).isEmailAlreadyInUse(anyString());
-    verify(studentRepository, never()).save(any(Student.class));
+    verify(studentRepository, never()).save(any(STUDENT_CLASS));
   }
 
   @Test
